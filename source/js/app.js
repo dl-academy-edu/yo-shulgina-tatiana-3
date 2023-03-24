2ecd90bacd1c385d2d16ec45867e6a1c28d753d3
TatianaShulgina2022 <118382447+TatianaShulgina2022@users.noreply.github.com>
2023-03-24 13:02:00 +0400

    типы данных

---
+++ source/js/app.js
@@ -0,0 +1,24 @@
+window.addEventListener('DOMContentLoaded', function () {
+
+});
+
+let abc = 150;
+let juice = "apple";
+let what = true;
+let none = null;
+let cripto = 124554n;
+let zero;
+let id = Symbol ("id");
+let cost = {
+    product : "juice",
+    price : 50,
+}
+
+console.log(Number(abc), String(abc), Boolean(abc));
+console.log(Number(juice), String(juice), Boolean(juice));
+console.log(Number(what), String(what), Boolean(what));
+console.log(Number(none), String(none), Boolean(none));
+console.log(Number(cripto), String(cripto), Boolean(cripto));
+console.log(Number(zero), String(zero), Boolean(zero));
+console.log(Number(cost), String(cost), Boolean(cost));
+
